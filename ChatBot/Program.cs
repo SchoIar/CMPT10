cd8e85c9c7434cc6f10176c890362a6f7b01dca0
Anton <93786227+SchoIar@users.noreply.github.com>
2021-11-05 12:30:29 -0700

    Add files via upload

---
+++ ChatBot/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ChatBotNumberOne
+{
+        class MainClass
+        {
+
+
+
+        public static void Main(string[] args)
+        {
+            string name;
+            Console.WriteLine("Hello Freind; What's your name?");
+            name = Console.ReadLine();
+
+            Console.WriteLine("How're you doing today " + name + " ?");
+            string input = Console.ReadLine();
+            switch (input.ToLower())
+            {
+                case "Good":
+                case "Okay":
+                    Console.WriteLine("Great, glad to hear that!");
+                    break;
+                case "Bad":
+                    Console.WriteLine("Well.. Thats why your here for theropy");
+                    break;
+                default:
+                    Console.WriteLine("I didnt catch that.. Anyways are you here for theropy");
+                    string input2 = Console.ReadLine();
+                    switch (input2.ToLower())
+                    {
+                        case "Yea":
+                        case "Yes":
+                            Console.WriteLine("Great! Your at the right place " + name + ".");
+                            break;
+                        case "no":
+                            Console.WriteLine("Oups.. Then your in the wrong place.");
+                            break;
+                        default:
+                            Console.WriteLine("I'm sorry, I don't understand that!");
+                            break;
+                    }
+                    break;
+            }
+        }
+    }
+}
